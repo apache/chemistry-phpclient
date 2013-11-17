5242cb2b2d942463ca655eca6828de8fcae74932
Richard McKnight <richardm@apache.org>
2013-11-17 23:22:10 +0000

    delete dist
    git-svn-id: https://svn.apache.org/repos/asf/chemistry/phpclient/trunk@1542868 13f79535-47bb-0310-9956-ffa450edef68

--- atom/create_release.sh
+++ atom/create_release.sh
@@ -1,3 +1,8 @@
-rm ../dist/cmis-phplib.tgz ../dist/cmis-phplib.zip
-tar -zcvf ../dist/cmis-phplib.tgz *.php cmis/*.php
-zip -v ../dist/cmis-phplib.zip *.php cmis/*.php
+rm ../../CMIS-LIB-DIST/cmis-phplib.tgz ../../CMIS-LIB-DIST/cmis-phplib.zip
+rm ../../CMIS-LIB-DIST/cmis-phplib.tgz.asc ../../CMIS-LIB-DIST/cmis-phplib.zip.asc
+tar -zcvf ../../CMIS-LIB-DIST/cmis-phplib.tgz *.php cmis/*.php
+zip -v ../../CMIS-LIB-DIST/cmis-phplib.zip *.php cmis/*.php
+cd ../../CMIS-LIB-DIST
+gpg --armor --output cmis-phplib.tgz.asc --detach-sig cmis-phplib.tgz
+gpg --armor --output cmis-phplib.zip.asc --detach-sig cmis-phplib.zip
+
