9997981436c39b4151f05ecdfadcd5fe46acec75
David Ramentol <david.ramentol@gmail.com>
2013-12-06 07:57:53 +0100

    Update cmis_service.php
    Fails creating custom models with its own properties. As I see it, the $objectType param must be used only when no objectTypeId is supplied in the $properties param.

--- atom/cmis/cmis_service.php
+++ atom/cmis/cmis_service.php
@@ -926,7 +926,7 @@ xmlns:cmisra="http://docs.oasis-open.org/ns/cmis/restatom/200908/">
 		if (!isset($hash_values["cmis:objectTypeId"])) {
 			$hash_values["cmis:objectTypeId"]=$objectType;
 		}
-		$properties_xml = $this->processPropertyTemplates($objectType,$hash_values);
+		$properties_xml = $this->processPropertyTemplates($hash_values["cmis:objectTypeId"],$hash_values);
 		if (is_array($options)) {
 			$hash_values=$options;
 		} else {
